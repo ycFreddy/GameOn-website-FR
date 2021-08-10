77c14f5009ca8d27a208a4d431608a985f40e9f8
ycFreddy <fredc@netc.fr>
2021-08-10 11:49:15 +0200

    v1.0

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,7 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formDatas = document.querySelectorAll(".formData");
 const modalClose = document.querySelector(".close");
+var displayErrLoc = 1;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -29,59 +30,72 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-// Traitement du Prénom
-const prenom = document.getElementById('first');
-const errPrenom = document.querySelector('.errPrenom');
-prenom.addEventListener('input', function(e) {  
-  if (e.target.value.length>=2) errPrenom.style.display = "none";
-  else errPrenom.style.display = "block";
-});
+/**
+ * traitement du formulaire
+ */
+formDatas.forEach(function (element) {  
+  NodeList.prototype.forEach = Array.prototype.forEach
+  var children = element.childNodes;
+  children.forEach(function(item){
+      switch (item.name) {
+        case "first" :
+          // Traitement du Prénom
+          const prenom = document.getElementById(item.name);
+          const errPrenom = document.querySelector('.errPrenom');
+          prenom.addEventListener('input', function(efirst) {  
+            if (efirst.target.value.length>=2) errPrenom.style.display = "none";
+            else errPrenom.style.display = "block";
+          });
+          break;
+        case "last" :
+          // Traitement du Nom
+          const nom = document.getElementById('last');
+          const errNom = document.querySelector('.errNom');
+          nom.addEventListener('input', function(e) {  
+            if (e.target.value.length>=2) errNom.style.display = "none";
+            else errNom.style.display = "block";
+          });
+          break;
+        case "email" :
+          // Traitement de l'email
+          const email = document.getElementById('email');
+          const errEmail = document.querySelector('.errEmail');
+          const regexpMail= /^[a-zA-Z0-9_-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/;
+          email.addEventListener('input', function(e) {
+            var testEmail = regexpMail.test(e.target.value);
+            if (testEmail) errEmail.style.display = "none";
+            else errEmail.style.display = "block";
+          });
+          break;
+        case "birthdate" :
+          // Traitement de la date d'anniversaire
+          const date = document.getElementById('birthdate');
+          const errDate = document.querySelector('.errDate');
+          const regexpDate = /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/;
+          date.addEventListener('input', function(e) {
+            var testDate = regexpDate.test(e.target.value);
+            if (testDate) errDate.style.display = "none";
+            else errDate.style.display = "block";
+          });
+          break;
+        case "quantity" :
+          // Traitement du Nombre de participation au tournois
+          const nbrTournois = document.getElementById('quantity');
+          const errNbr = document.querySelector('.errNbr');
+          nbrTournois.addEventListener('input', function(e) {  
+            if (e.target.value>=0) errNbr.style.display = "none";
+            else errNbr.style.display = "block";
+          });
+          break;
+        case "location" :
 
-// Traitement du Nom
-const nom = document.getElementById('last');
-const errNom = document.querySelector('.errNom');
-nom.addEventListener('input', function(e) {  
-  if (e.target.value.length>=2) errNom.style.display = "none";
-  else errNom.style.display = "block";
-});
+          break;
+        case "cu" :
 
-// Traitement de l'email
-const email = document.getElementById('email');
-const errEmail = document.querySelector('.errEmail');
-email.addEventListener('input', function(e) { 
-  var testEmail = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/.test(e.target.value);
-  if (testEmail) errEmail.style.display = "none";
-  else errEmail.style.display = "block";
+          break;
+    }
+  });
 });
-
-// Traitement de la date d'anniversaire
-const date = document.getElementById('birthdate');
-const errDate = document.querySelector('.errDate');
-date.addEventListener('input', function(e) {
-  var testDate = /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/.test(e.target.value);
-  if (testDate) errDate.style.display = "none";
-  else errDate.style.display = "block";
-});
-
-// Traitement du Nombre de participation au tournois
-const nbrTournois = document.getElementById('quantity');
-const errNbr = document.querySelector('.errNbr');
-nbrTournois.addEventListener('input', function(e) {  
-  if (e.target.value>=0) errNbr.style.display = "none";
-  else errNbr.style.display = "block";
-});
-
-// Traitement du lieu
-var displayErrLoc = 1;
-const radios = document.querySelectorAll('input[name="location"]');
-const errLocMsg = document.querySelector('.errlocation');
-radios.forEach((elem) => {  
-  elem.addEventListener("change", checkradio);
-});
-function checkradio() {
-  errLocMsg.style.display = "none";
-  displayErrLoc = 0;
-};
 
 // Traitement des conditions d'utilisation
 const cu = document.getElementById("checkbox1");
@@ -92,16 +106,34 @@ function clickcu() {
   else errCuMsg.style.display = "none";
 }
 
-// Vérification du formulaire
+// Traitement du lieu
+var displayErrLoc = 1;
+//const radios = document.querySelectorAll('input[name="location"]');
+const radios = document.getElementsByName("location");
+const errLocMsg = document.querySelector('.errlocation');
+radios.forEach((elem) => {  
+  elem.addEventListener("change", checkradio);
+});
+function checkradio() {
+  errLocMsg.style.display = "none";
+  displayErrLoc = 0;
+};
+
+
+/**
+* Vérification du formulaire
+*/
 function validate(event) {  
   // Vérification de location  
-  if (displayErrLoc !=0){
+  if (displayErrLoc != 0){
     errLocMsg.style.display = "block";    
     event.preventDefault();
+    if(event.preventDefault) alert("toto");
   }
   else {
     var lieuValide = 1;
   }
+  
   // Vérification Condition d'utilisation
   if (cu.checked == false){
     errCuMsg.style.display = "block";    
